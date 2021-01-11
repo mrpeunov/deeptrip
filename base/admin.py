757eb24dfeb4abaeca8c9ec3868ed72d43565b17
Vitaly <mr.peunov@gmail.com>
2021-01-11 18:33:51 +0300

    Work with git error

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Config)
+class ConfigAdmin(admin.ModelAdmin):
+    list_display = ('site_name',)
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'tours_count', 'orders_count')
+    prepopulated_fields = {"slug": ("name", )}
+    actions_on_top = False
+    actions_on_bottom = True
+
+
+@admin.register(SocialNetwork)
+class SocialNetworkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'link')
--- maps/models.py
+++ maps/models.py
@@ -5,3 +5,10 @@ class Position(models.Model):
     name = models.CharField("Название позиции", max_length=32)
     lat = models.FloatField("Широта")
     lon = models.FloatField("Долгота")
+
+    def __str__(self):
+        return "Точка '{}'".format(self.name)
+
+    class Meta:
+        verbose_name = "точка на карте"
+        verbose_name_plural = "точки на карте"
