7b371c846e5b100fda0c0458385c51e9f439d877
Vitaly <mr.peunov@gmail.com>
2021-03-17 14:26:05 +0300

    Almost done popup om tour page0

--- static/scripts/tour/popups.js
+++ static/scripts/tour/popups.js
@@ -1,14 +1,31 @@
+
+let $comment_popup = $('#comment_popup');
+
 $('#send_comment').on('click', function () {
-    $('#comment_popup').css("display", "flex");
+    open_popup_comment();
 })
 
-$('#comment_popup').on('click', function () {
-    $('#comment_popup').css("display", "none");
+$comment_popup.on('click', function () {
+    close_popup_comment();
 })
 
 $('#comment_popup_form').on('click', function (event){
     event.stopPropagation();
 })
+
+$('#tour_popup_close').on('click', function (){
+    close_popup_comment();
+})
+
+function open_popup_comment(){
+    $comment_popup.css("display", "flex");
+    $('body').css("overflow", "hidden");
+}
+
+function close_popup_comment(){
+    $comment_popup.css("display", "none");
+    $('body').removeAttr("style");
+}
 
 $('.tour_popup_comment_rating_items_item').on('click', function (){
     $('.tour_popup_comment_rating_items_item').removeClass('active');
