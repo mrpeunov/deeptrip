de5d9208a3e4bab24655e6de13fb2c6a635f9e59
Vitaly <mr.peunov@gmail.com>
2021-03-23 16:28:32 +0300

    Done copy link

---
+++ static/scripts/tour/copy.js
@@ -0,0 +1,11 @@
+$('#copy').on('click', function () {
+    //копирование ссылки в буфер обмена
+    let text = document.createElement('input');
+
+    //берём текущую ссылку
+    text.value = window.location.href;
+    document.body.appendChild(text);
+    text.select();
+    document.execCommand('copy');
+    document.body.removeChild(text);
+})
