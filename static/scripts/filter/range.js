4621b3d97ef52ae7fcdee95f951ef480a28d03ce
Vitaly <mr.peunov@gmail.com>
2021-04-29 14:47:34 +0300

    All frontend

--- static/scripts/filter/range.js
+++ static/scripts/filter/range.js
@@ -4,7 +4,7 @@ $(function() {
           min: 0,
           max: 500,
           step: 10,
-          values: [ 75, 300 ],
+          values: [ 0, 500 ],
           slide: function( event, ui ) {
               $("#min").html($( "#slider-range" ).slider( "values", 0));
               $("#max").html($( "#slider-range" ).slider( "values", 1));
